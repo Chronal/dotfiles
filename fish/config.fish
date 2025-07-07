b22a54a10515145910828792f7068c26628ee247
Anuj Dhavalikar <chronal@chronal.space>
2025-07-07 13:14:58 +1000

    Reorganises fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,19 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# BEGIN opam configuration
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
-test -r '/home/chronal/.opam/opam-init/init.fish' && source '/home/chronal/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
-# END opam configuration
-
 # Environment Variables
 set -gx EDITOR vim
 set -gx VISUAL emacsclient --create-frame --alternate-editor=vim
 
-set -gx LD_LIBRARY_PATH "/usr/lib64" "/usr/local/lib64" "/usr/local/lib/" "$HOME/.local/lib"
+set -gx LD_LIBRARY_PATH "$HOME/.local/lib" /usr/local/lib /usr/local/lib64
 
 ## XDG
 set -gx XDG_CONFIG_HOME "$HOME/.config"
@@ -22,12 +14,17 @@ set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -gx XDG_STATE_HOME "$HOME/.local/state"
 
 ## pnpm
-set -gx PNPM_HOME "/home/chronal/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
+set -gx PNPM_HOME $XDG_DATA_HOME/pnpm
 
 # Adding things to $PATH
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
+fish_add_path $PNPM_HOME
+
+# BEGIN opam configuration, only needed for Rocq
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/chronal/.opam/opam-init/init.fish' && source '/home/chronal/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
+# END opam configuration
